e641c8d177509500f1433edc78a81e0196412cc7
Schanzenbach, Martin <mschanzenbach@posteo.de>
2019-04-28 14:21:22 +0200

    more cleanup

--- target/demo.rb
+++ target/demo.rb
@@ -47,7 +47,7 @@ begin
   uri = URI.parse("#{$reclaim_endpoint}/identity/name/reclaim")
   req = Net::HTTP::Get.new(uri)
   Net::HTTP.SOCKSProxy($reclaim_runtime, 7777).start(uri.host, uri.port, :use_ssl => true,
-                                                           :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+                                                     :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
     resp = http.request(req).body
     puts resp
     $demo_pkey = JSON.parse(resp)["pubkey"]
@@ -67,7 +67,7 @@ def oidc_token_request(authz_code)
     req = Net::HTTP::Post.new(uri)
     req.basic_auth $demo_pkey, $client_secret
     Net::HTTP.SOCKSProxy($reclaim_runtime, 7777).start(uri.host, uri.port, :use_ssl => true,
-                                                             :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+                                                       :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
       return http.request(req).body
     end
   rescue Exception => e
@@ -77,213 +77,195 @@ def oidc_token_request(authz_code)
   end
 end
 
-def exchange_code_for_token(id_ticket, expected_nonce)
-    #cmd = "curl -X POST --socks5-hostname #{ENV['RECLAIM_RUNTIME']}:7777 'https://api.reclaim/openid/token?grant_type=authorization_code&redirect_uri=https://demo.#{$demo_pkey}/login&code=#{CGI.escape(id_ticket)}' -u #{$demo_pkey}:#{ENV["PSW_SECRET"]}"
-    #p "Executing: "+cmd
+def parse_token_response(response)
+  begin
+    json = JSON.parse(resp)
+  rescue JSON::ParserError
+    puts "ERROR: Unable to parse JSON"
+    return nil
+  end
+  raise "JSON is empty" if json.nil? or json.empty?
+  id_jwt = json["id_token"]
+  raise "No ID Token" if id_token.nil?
+  access_token = json["access_token"]
+  begin
+    #                      JWT     pwd  validation (have no key)
+    id_token = JWT.decode(id_jwt, $client_secret, true,  {algorithm: 'HS512' })
+    payload = id_jwt[0] # 0 is payload, 1 is header
+  rescue
+    puts "ERROR: Unable to decode JWT"
+    return nil
+  end
+  return {:access_token => access_token, :id_token => id_token}
+end
 
-    #resp = `#{cmd}`
-    resp = oidc_token_request(id_ticket)
-    puts resp
+def exchange_code_for_token(code, expected_nonce)
+  #cmd = "curl -X POST --socks5-hostname #{ENV['RECLAIM_RUNTIME']}:7777 'https://api.reclaim/openid/token?grant_type=authorization_code&redirect_uri=https://demo.#{$demo_pkey}/login&code=#{CGI.escape(id_ticket)}' -u #{$demo_pkey}:#{ENV["PSW_SECRET"]}"
+  #p "Executing: "+cmd
 
+  #resp = `#{cmd}`
+  resp = oidc_token_request(code)
+  puts resp
+
+  tokens = parse_token_response(token)
+  raise "ERROR: unable to parse tokens!" if tokens.nil?
+  payload = tokens[:id_token][0] # 0 is payload, 1 is header
+  identity = payload["iss"]
+  $knownIdentities[identity] = payload
+
+  #Async retrieval of userinfo
+  Thread.new do
+    #resp = `curl -X POST --socks5-hostname '#{ENV['RECLAIM_RUNTIME']}':7777 'https://api.reclaim/openid/userinfo' -H 'Authorization: Bearer #{access_token}'`
     begin
-      json = JSON.parse(resp)
-    rescue JSON::ParserError
-      puts "ERROR: Unable to parse JSON"
-      return nil
-    end
-    return nil if json.nil? or json.empty?
-    id_token = json["id_token"]
-    return nil if id_token.nil?
-    access_token = json["access_token"]
-    begin
-      #                      JWT     pwd  validation (have no key)
-      payload = JWT.decode(id_token, $client_secret, true,  {algorithm: 'HS512' })[0] # 0 is payload, 1 is header
-    rescue
-      puts "ERROR: Unable to decode JWT"
-      return nil
-    end
-    identity = payload["iss"]
-    $knownIdentities[identity] = payload
-
-    #Async retrieval of userinfo
-    Thread.new do
-      #resp = `curl -X POST --socks5-hostname '#{ENV['RECLAIM_RUNTIME']}':7777 'https://api.reclaim/openid/userinfo' -H 'Authorization: Bearer #{access_token}'`
-      begin
-        uri = URI.parse($userinfo_endpoint)
-        req = Net::HTTP::Post.new(uri)
-        req['Authorization'] = "Bearer #{access_token}"
-        Net::HTTP.SOCKSProxy($reclaim_runtime, 7777).start(uri.host, uri.port, :use_ssl => true,
-                     :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
-          resp = http.request(req)
-          puts resp
-          $knownIdentities[identity] = JSON.parse(resp)
-          puts "Got Userinfo: #{$knownIdentities[identity]}"
-        end
-      rescue JSON::ParserError
-        puts "ERROR: Unable to retrieve Userinfo! Using ID Token contents..."
-      rescue
-        puts "ERROR: Userinfo request failed!"
+      puts "Getting Userinfo..."
+      uri = URI.parse($userinfo_endpoint)
+      req = Net::HTTP::Post.new(uri)
+      req['Authorization'] = "Bearer #{tokens[:access_token]}"
+      Net::HTTP.SOCKSProxy($reclaim_runtime, 7777).start(uri.host, uri.port, :use_ssl => true,
+                                                         :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+        resp = http.request(req)
+        puts resp
+        $knownIdentities[identity] = JSON.parse(resp)
+        puts "Userinfo: #{$knownIdentities[identity]}"
       end
+    rescue JSON::ParserError
+      puts "ERROR: Unable to retrieve Userinfo! Using ID Token contents..."
+    rescue
+      puts "ERROR: Userinfo request failed!"
     end
-    if expected_nonce != payload["nonce"].to_i
-      puts "ERROR: Expected nonce #{expected_nonce} != #{payload["nonce"].to_i}"
-      return nil
-    end
+  end
+  raise "Expected nonce #{expected_nonce} != #{payload["nonce"].to_i}" if expected_nonce != payload["nonce"].to_i
 
-    $tokens[identity] = id_token
-    $codes[identity] = id_ticket
-    return identity
+  $tokens[identity] = tokens[:id_token]
+  $codes[identity] = code
+  return identity
 end
 
-def is_token_expired (token)
-    return true # TODO if token.nil?
-    identity = $knownIdentities[token["iss"]]
-    exp = Time.at(token["exp"] / 1000000)
-    if (Time.now > exp)
-        # Get new token
-        new_token = `gnunet-gns -u #{$codes[identity]}.gnu -p #{token["iss"]} -t ID_TOKEN --raw -T 5000`
-        if (new_token.nil? or new_token.empty?)
-            $knownIdentities[token["iss"]] = nil
-            return true
-        end
-        new_token = JSON.parse(new_token)
-        exp = Time.at(new_token["exp"] / 1000000)
-        if (Time.now > exp)
-            $knownIdentities[token["iss"]] = nil
-            return true
-        else
-            $knownIdentities[token["iss"]] = new_token
-            return false
-        end
-    else
-        # Check if token revoked
-        return false
-    end
-end
 
 def logout()
   session["user"] = nil
 end
 
 get '/logout' do
-    logout()
-    redirect to('/login')
+  logout()
+  redirect to('/login')
 end
 
 def getUser(identity)
-    return nil if identity.nil? or $knownIdentities[identity].nil?
-    return $knownIdentities[identity]["full_name"] unless $knownIdentities[identity]["full_name"].nil?
-    return $knownIdentities[identity]["sub"]
+  return nil if identity.nil? or $knownIdentities[identity].nil?
+  return $knownIdentities[identity]["full_name"] unless $knownIdentities[identity]["full_name"].nil?
+  return $knownIdentities[identity]["sub"]
 end
 
 get '/' do
-    identity = session["user"]
+  identity = session["user"]
 
-    if (!identity.nil?)
-        token = $knownIdentities[identity]
-        if (!token.nil?)
-            email = token["email"]
-            return haml :info, :locals => {
-                :user => getUser(identity),
-                :title => "Welcome.",
-                :subtitle => "Welcome back #{$knownIdentities[identity]["full_name"]} (#{email})",
-                :content => ""}
-        end
+  if (!identity.nil?)
+    token = $knownIdentities[identity]
+    if (!token.nil?)
+      email = token["email"]
+      return haml :info, :locals => {
+        :user => getUser(identity),
+        :title => "Welcome.",
+        :subtitle => "Welcome back #{$knownIdentities[identity]["full_name"]} (#{email})",
+        :content => ""}
     end
+  end
 
-    redirect "/login"
+  redirect "/login"
 end
 
 get "/access_denied" do
-    return haml :access_denied, :locals => {
-        :user => getUser(nil),
-        :title => "Error",
-        :subtitle => "Access was denied",
-        :content => "You have chosen to deny access to share your identity with us.<br \>
+  return haml :access_denied, :locals => {
+    :user => getUser(nil),
+    :title => "Error",
+    :subtitle => "Access was denied",
+    :content => "You have chosen to deny access to share your identity with us.<br \>
         You can try again by clicking the button below.<br \>
         (The chosen identity provider supplied the following error decription: #{params["error_description"]})"}
 end
 
 get "/login" do
-    identity = session["user"]
-    token = params[:id_token]
-    id_ticket = params[:code]
+  identity = session["user"]
+  token = params[:id_token]
+  id_ticket = params[:code]
 
-    if(params["error"] == 'access_denied')
-        redirect "/access_denied?error_description=#{params["error_description"]}"
-    else
-        if (params["error"] != nil)
-            p params["error"]
-            p "ERROR! unhandled/unexpected error occurred"
-            redirect "/"
-        end
+  if(params["error"] == 'access_denied')
+    redirect "/access_denied?error_description=#{params["error_description"]}"
+  else
+    if (params["error"] != nil)
+      p params["error"]
+      p "ERROR! unhandled/unexpected error occurred"
+      redirect "/"
+    end
+  end
+
+  if (!identity.nil?)
+    token = $knownIdentities[identity]
+    p token
+
+    if (!token.nil?)
+      redirect "/"
     end
 
-    if (!identity.nil?)
-        token = $knownIdentities[identity]
-        p token
+  end
 
-        if (!token.nil?)
-            redirect "/"
-        end
-
-    end
-
-    if (!id_ticket.nil?)
-        identity = exchange_code_for_token(id_ticket, $nonces[session["id"]])
-        $nonces[session["id"]] = nil
-        if (identity.nil?)
-            return "Error!"
-        end
-        token = $knownIdentities[identity]
-        email = $knownIdentities[identity]["email"]
-        session["user"] = identity
-        if (email.nil?)
-          #return "You did not provide a valid email. Please grant us access to your email!<br/> <a href=https://ui.reclaim/#/identities/#{identity}?requested_by=http%3A//demo.reclaim/&requested_attrs=phone>Grant access</a>"
-          logout()
-          return haml :login, :locals => {
-            :user => getUser(nil),
-            :title => "Login",
-            :subtitle => "You did not provide a valid email. Please grant us access to your email!",
-            :nonce => nonce,
-            :reclaim_endpoint => $reclaim_endpoint,
-            :demo_pkey => $demo_pkey
-          }
-        end
-        #Handle token contents
-        redirect "/"
-    elsif (identity.nil?)
-        nonce = rand(100000)
-        session["id"] = rand(100000)
-        $nonces[session["id"]] = nonce
+  if (!id_ticket.nil?)
+    begin
+      identity = exchange_code_for_token(id_ticket, $nonces[session["id"]])
+      $nonces[session["id"]] = nil
+      token = $knownIdentities[identity]
+      email = $knownIdentities[identity]["email"]
+      session["user"] = identity
+      if (email.nil?)
+        #return "You did not provide a valid email. Please grant us access to your email!<br/> <a href=https://ui.reclaim/#/identities/#{identity}?requested_by=http%3A//demo.reclaim/&requested_attrs=phone>Grant access</a>"
+        logout()
         return haml :login, :locals => {
-            :user => getUser(nil),
-            :title => "Login",
-            :subtitle => "To use the re:claim messaging board, you must first authenticate yourself!",
-            :nonce => nonce,
-            :reclaim_endpoint => $reclaim_endpoint,
-            :demo_pkey => $demo_pkey
+          :user => getUser(nil),
+          :title => "Login",
+          :subtitle => "You did not provide a valid email. Please grant us access to your email!",
+          :nonce => nonce,
+          :reclaim_endpoint => $reclaim_endpoint,
+          :demo_pkey => $demo_pkey
         }
+      end
+      #Handle token contents
+      redirect "/"
+    rescue Exception => e
+      return e.message
     end
+  elsif (identity.nil?)
+    nonce = rand(100000)
+    session["id"] = rand(100000)
+    $nonces[session["id"]] = nonce
+    return haml :login, :locals => {
+      :user => getUser(nil),
+      :title => "Login",
+      :subtitle => "To use the re:claim messaging board, you must first authenticate yourself!",
+      :nonce => nonce,
+      :reclaim_endpoint => $reclaim_endpoint,
+      :demo_pkey => $demo_pkey
+    }
+  end
 end
 
 get "/submit" do
-    identity = session["user"]
+  identity = session["user"]
 
-    if (!identity.nil?)
-        token = $knownIdentities[identity]
-        if (!token.nil?)
-            email = token["email"]
-            begin
-                file = File.open("guestbook.txt", "a")
-                file.write("<tr><td><a href=\"mailto:"+email+"\">"+$knownIdentities[identity]["full_name"]+"</a></td><td>"+params["message"]+"</td></tr>")
-            rescue IOError => e
-            ensure
-                file.close unless file.nil?
-            end
-            redirect("/")
-        end
+  if (!identity.nil?)
+    token = $knownIdentities[identity]
+    if (!token.nil?)
+      email = token["email"]
+      begin
+        file = File.open("guestbook.txt", "a")
+        file.write("<tr><td><a href=\"mailto:"+email+"\">"+$knownIdentities[identity]["full_name"]+"</a></td><td>"+params["message"]+"</td></tr>")
+      rescue IOError => e
+      ensure
+        file.close unless file.nil?
+      end
+      redirect("/")
     end
+  end
 
 end
 
@@ -291,5 +273,5 @@ end
 # catch-all error handler
 # redirect back to main page (login) in case of errors
 error do
-    redirect("/")
+  redirect("/")
 end
